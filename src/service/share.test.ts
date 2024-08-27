6bf488d94e2a0ee48f266a3c23459cb54682a3a4
Zxilly <zxilly@outlook.com>
2024-08-28 04:30:52 +0800

    test: fix for new api

--- src/service/share.test.ts
+++ src/service/share.test.ts
@@ -4,12 +4,13 @@ import { generateDataShareUrl, loadShareCode } from '@/service/share'
 it.each([
   ['print(\'Hello, world!\')', 'http://localhost:3000/#data=A4JwlgdgLgFA5ACQKYBsUHsA0ACA7ukFAEwEI4BKIA'],
   ['print(\'中文代码\')', 'http://localhost:3000/#data=A4JwlgdgLgFA5IWjlDhpoY7lCAHnAlEA'],
-])('test share serialization with code: %s', (code, expectedUrl) => {
+])('test share serialization with code: %s', async (code, expectedUrl) => {
   const url = generateDataShareUrl(code)
 
   expect(url).toBe(expectedUrl)
 
   window.location.href = url
-  const newCode = loadShareCode()
+  const [newCode, success] = await loadShareCode()
   expect(newCode).toBe(code)
+  expect(success).toBe(true)
 })
