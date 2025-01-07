dcc0e3d2ec9b4b91454706c9df7c4b6b8d9d025f
Zxilly <zxilly@outlook.com>
2025-01-07 19:05:16 +0800

    feat: switch to woff2

--- codapi/README.md
+++
@@ -1,3 +0,0 @@
-# Codapi 配置文件
-
-后端使用 [Codapi](https://github.com/nalgeon/codapi) 作为 API 服务。
--- src/app/font.tsx
+++ src/app/font.tsx
@@ -1,13 +1,13 @@
 import localFont from 'next/font/local'
 
 export const harmonyFont = localFont({
-  src: './fonts/HarmonyOS_Sans.ttf',
-  preload: false,
+  src: './fonts/HarmonyOS_Sans.woff2',
+  preload: true,
 })
 
 export const jetbrainsFont = localFont({
-  src: './fonts/JetBrainsMono.ttf',
-  preload: false,
+  src: './fonts/JetBrainsMono.woff2',
+  preload: true,
 })
 
 export const fontFamily = `${jetbrainsFont.style.fontFamily}, ${harmonyFont.style.fontFamily}, sans-serif`
