7366a6e3cca8060ded251031b1800cf8d844a6fe
Zxilly <zxilly@outlook.com>
2026-01-15 21:55:55 +0800

    feat: add crossOrigin attribute to TrackingScript components for improved security

--- src/components/TrackingScript.tsx
+++ src/components/TrackingScript.tsx
@@ -11,12 +11,14 @@ function TrackingScript() {
             defer
             src="https://trail.learningman.top/script.js"
             data-website-id="2cd9ea13-296b-4d90-998a-bbbc5613fc20"
+            crossOrigin="anonymous"
           />
           <Script
             id="cf-beacon"
             defer
             src="https://static.cloudflareinsights.com/beacon.min.js"
             data-cf-beacon='{"token": "2622f32f6909497cae2680dad39f4c21"}'
+            crossOrigin="anonymous"
           />
         </Fragment>
       )}
